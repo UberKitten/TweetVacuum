630ee1011897ceabab60cd67b354d18a8b570d45
UberKitten <566438+UberKitten@users.noreply.github.com>
2016-09-02 04:16:12 -0500

    Add auto scroll to inject code

--- inject.js
+++ inject.js
@@ -2,3 +2,7 @@ $(".topbar").remove();
 $("#page-container").css("paddingTop", "0px");
 $(".AdaptiveFiltersBar").remove();
 $(".SidebarCommonModules").parent().remove();
+
+setInterval(function() {
+	window.scrollTo(0, document.body.scrollHeight);
+}, 1000);
