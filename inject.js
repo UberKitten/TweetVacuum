b44b0d2365abf9cfd6b7bedd57740b171f3eca1c
UberKitten <566438+UberKitten@users.noreply.github.com>
2016-09-05 03:48:53 -0500

    Refactor inject.js to autostart after jQuery is loaded

--- inject.js
+++ inject.js
@@ -1,36 +1,44 @@
-$(".topbar").remove();
-$("#page-container").css("paddingTop", "0px");
-$(".AdaptiveFiltersBar").remove();
-$(".SidebarCommonModules").parent().remove();
-// We need this so that we can scroll to the bottom and trigger Twitter's infinite scroll code
-$("#page-container").append($("<div>", {
-	style: "height: 10000px; width: 1px;"
-}));
-
-var db;
-
-function run(username) {
-	db = createDb(username);
-	setTimeout(loop, 1000);
-	setInterval(scroll, 500);
-}
-
+var loaded = false;
 function loop() {
-	items = $(".js-stream-item")
-		.not("[vacuumed=true]")
-		.each(function() {
-			var item = $(this);
+	if (loaded) {
+		items = $(".js-stream-item")
+			.not("[vacuumed=true]")
+			.each(function() {
+				var item = $(this);
+				
+				chrome.runtime.sendMessage({
+					action: "add",
+					data: {
+						tweetId: item.attr("data-item-id"),
+						date: parseInt(item.find(".js-short-timestamp").attr("data-time-ms"))
+					}
+				});
+				item.attr("vacuumed", true);
+				item.hide();
+		});
+	} else {
+		if (window.jQuery) {
+			loaded = true;
 			
-			chrome.runtime.sendMessage({
-				tweetId: item.attr("data-item-id"),
-				date: parseInt(item.find(".js-short-timestamp").attr("data-time-ms"))
-			});
-			item.attr("vacuumed", true);
-			item.hide();
-	});
+			$(".topbar").remove();
+			$("#page-container").css("paddingTop", "0px");
+			$(".AdaptiveFiltersBar").remove();
+			$(".SidebarCommonModules").parent().remove();
+			// We need this so that we can scroll to the bottom and trigger Twitter's infinite scroll code
+			$("#page-container").append($("<div>", {
+				style: "height: 10000px; width: 1px;"
+			}));
+			
+			setInterval(scroll, 500);
+		}
+	}
 	
 	setTimeout(loop, 1000);
 }
+
+// Prefer to use setTimeout to not have multiple occurences of loop()
+// running at the same time
+setTimeout(loop, 1000);
 
 var up;
 function scroll() {
@@ -44,14 +52,9 @@ function scroll() {
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
     if (request.action == "stop") {
+		log("Stop request received");
 		db.close();
 		window.close();
-	}
-	if (request.action == "start") {
-		run(request.username);
 	}
   });
