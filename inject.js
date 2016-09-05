02634f63a890d040b820abebc6e14f9a23c64403
UberKitten <566438+UberKitten@users.noreply.github.com>
2016-09-05 04:08:49 -0500

    Remove db reference in inject.js stop function

--- inject.js
+++ inject.js
@@ -53,7 +53,6 @@ function scroll() {
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.action == "stop") {
-		db.close();
 		window.close();
 	}
   });
